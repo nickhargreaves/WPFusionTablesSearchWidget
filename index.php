8a99d24c720d5c8dcb044dd7d798d21a45cb884f
Nick Hargreaves <hargreaves1234@gmail.com>
2015-08-29 10:30:19 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: WP Fusion Tables Search
+ * Plugin URI: http://github.com/nickhargreaves/wpfusiontablessearch
+ * Description: This plugin allows you to create a widget for your site visitors to easily to search a Fusion Tables database.
+ * Version: 1.0.0
+ * Author: Nick Hargreaves
+ * Author URI: http://nickhargreaves.com
+ * License: GPL2
+ */
+
+
+?>
