3311ba97746d81ceaa97acb7b8e4e35f340d5f5f
Nick Hargreaves <hargreaves1234@gmail.com>
2015-08-29 11:11:25 +0300

    add settings page and api key input box

--- inc/add_menu.php
+++ inc/add_menu.php
@@ -11,6 +11,8 @@ if ( !current_user_can( 'manage_options' ) )  {
 wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
 }
 echo '<div class="wrap">';
-    echo '<p>Here is where the form would go if I actually had options.</p>';
+
+    require_once("config_options.php");
+
     echo '</div>';
 }
